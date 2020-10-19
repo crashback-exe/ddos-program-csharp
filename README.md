0ca36b256d970eb0443bd7d29dc7e493a6084453
crashback-exe <giacomo.abbruciati@hotmail.com>
2020-10-19 19:48:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DDoS Program in C# made by crashback-exe
+
+Feel free to use it for every purpose... :P
+
+###### This was made by joke, no offence is intended, the only reason i did this is to introduce myself in c sharp... Have a great day! 
---
+++ src/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+
+namespace csharp_hello_world
+{
+    class Program
+    {
+
+        private static int percentage = 0;
+
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            
+            Console.Write("Insert ip to DDoS: ");
+            string ip = Console.ReadLine();
+
+            Console.Write($"\nDDoS Attacking {ip}");
+            
+
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+
+            Console.Write("\b");
+            Thread.Sleep(1000);
+            Console.Write("\b");
+            Thread.Sleep(1000);
+            Console.Write("\b");
+            Thread.Sleep(1000);
+
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(1000);
+            Console.Write(".");
+            Thread.Sleep(5000);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nNote: This attack will take about 10 seconds");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            for(int i = 0; i<11; i++){
+
+                Console.WriteLine($"DDoS attack {percentage}%");
+                percentage += 10;
+                Thread.Sleep(1000);
+                
+                }
+            Thread.Sleep(10000);
+            Console.WriteLine($"DDoS to {ip} complete");
+        }
+    }
+}
